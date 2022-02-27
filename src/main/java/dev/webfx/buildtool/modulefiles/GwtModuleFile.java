0deb1a8fff606e134fc861d906e3b28df91c07f6
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-27 21:39:41 +0000

    Cleared the unnecessary comments when generating the GWT modules (module.gwt.xml files)

--- src/main/java/dev/webfx/buildtool/modulefiles/GwtModuleFile.java
+++ src/main/java/dev/webfx/buildtool/modulefiles/GwtModuleFile.java
@@ -1,16 +1,19 @@
 package dev.webfx.buildtool.modulefiles;
 
-import dev.webfx.buildtool.util.textfile.ResourceTextFileReader;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import dev.webfx.buildtool.Module;
 import dev.webfx.buildtool.ModuleDependency;
 import dev.webfx.buildtool.ProjectModule;
+import dev.webfx.buildtool.util.textfile.ResourceTextFileReader;
 import dev.webfx.buildtool.util.xml.XmlUtil;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -40,29 +43,51 @@ public final class GwtModuleFile extends XmlModuleFile {
         document.getDocumentElement().setAttribute("rename-to", getModule().getName().replaceAll("-", "_"));
         Node moduleSourceCommentNode = lookupNode("/module//comment()[2]");
         Node moduleSourceEndNode = moduleSourceCommentNode.getNextSibling();
+        // In the GWT module file, we need to list all transitive dependencies (not only direct dependencies) so that GWT can find all the sources required by the application
         getProjectModule().getTransitiveDependencies()
                 .stream().collect(Collectors.groupingBy(ModuleDependency::getDestinationModule)).entrySet()
                 .stream().sorted(Map.Entry.comparingByKey())
                 .forEach(moduleGroup -> {
                     Module module = moduleGroup.getKey();
-                    // Ignoring emulated modules because 1) they are destined to the super source, not the source (so they don't need to be listed here) and 2) these modules have been shaded so the original source packages would start with emul (which would be incorrect) if they were listed here
+                    // Ignoring emulated modules for 2 reasons:
+                    // 1) they are destined to the super source, not the source (so they don't need to be listed here)
+                    // 2) these modules have been shaded so the original source packages would start with emul (which would be incorrect) if they were listed here
                     if (module.getName().startsWith("webfx-platform-gwt-emul-"))
                         return;
                     Node parentNode = moduleSourceEndNode.getParentNode();
+                    // Creating a node appender that inserts a node with the after a new line and indentation
                     String newIndentedLine = "\n    ";
                     Consumer<Node> nodeAppender = node -> {
                         parentNode.insertBefore(document.createTextNode(newIndentedLine), moduleSourceEndNode);
                         parentNode.insertBefore(node, moduleSourceEndNode);
                     };
-                    nodeAppender.accept(document.createComment(createModuleSectionLine(module.getName())));
+                    // Creating a wrapper of the node appender that will drop all the unnecessary comments if there is
+                    // finally no elements (such as source, resource or configuration property) for that module.
+                    List<Comment> initialComments = new ArrayList<>();
+                    Boolean[] hasOnlyCommentsSoFar = {true}; // wrapping the boolean in an array so the lambda expression can change its value
+                    Consumer<Node> nodeAppenderIfNotOnlyComment = node -> {
+                        if (hasOnlyCommentsSoFar[0]) {
+                            if (node instanceof Comment) {
+                                initialComments.add((Comment) node);
+                                return;
+                            }
+                            // First node that is not a comment
+                            initialComments.forEach(nodeAppender); // Writing back all initial comments
+                            initialComments.clear(); // Ok to free the memory now
+                            hasOnlyCommentsSoFar[0] = false; // Changing the boolean value
+                            // Now that all comments are written, we can append the node
+                        }
+                        nodeAppender.accept(node);
+                    };
+                    nodeAppenderIfNotOnlyComment.accept(document.createComment(createModuleSectionLine(module.getName())));
                     moduleGroup.getValue()
                             .stream().sorted(Comparator.comparing(ModuleDependency::getSourceModule)) // Sorting by source module name instead of default (destination module name)
-                            .forEach(dep -> nodeAppender.accept(document.createComment(" used by " + dep.getSourceModule() + " (" + dep.getType() + ") ")));
+                            .forEach(dep -> nodeAppenderIfNotOnlyComment.accept(document.createComment(" used by " + dep.getSourceModule() + " (" + dep.getType() + ") ")));
                     String gwtModuleName = getGwtModuleName(module);
                     if (gwtModuleName != null) {
                         Element inherits = document.createElement("inherits");
                         inherits.setAttribute("name", gwtModuleName);
-                        nodeAppender.accept(inherits);
+                        nodeAppenderIfNotOnlyComment.accept(inherits);
                     }
                     if (module instanceof ProjectModule) {
                         ProjectModule projectModule = (ProjectModule) module;
@@ -71,28 +96,30 @@ public final class GwtModuleFile extends XmlModuleFile {
                                 .forEach(p -> {
                                     Element sourceElement = document.createElement("source");
                                     sourceElement.setAttribute("path", p.replaceAll("\\.", "/"));
-                                    nodeAppender.accept(sourceElement);
+                                    nodeAppenderIfNotOnlyComment.accept(sourceElement);
                                 });
                         projectModule.getResourcePackages()
                                 .stream().sorted()
                                 .forEach(p -> {
                                     Element resourceElement = document.createElement("resource");
                                     resourceElement.setAttribute("path", p.replaceAll("\\.", "/"));
-                                    nodeAppender.accept(resourceElement);
+                                    nodeAppenderIfNotOnlyComment.accept(resourceElement);
                                     Element publicElement = document.createElement("public");
                                     publicElement.setAttribute("path", "");
                                     publicElement.setAttribute("includes", p.replaceAll("\\.", "/") + "/");
-                                    nodeAppender.accept(publicElement);
+                                    nodeAppenderIfNotOnlyComment.accept(publicElement);
                                 });
-                        /* Declaring the system properties to ask GWT to replace the System.getProperty() calls with the values at compile time
-                           Note: these properties are set with the -setProperty GWT compiler argument when calling the GWT plugin in the root pom.xml */
+                        // Declaring the system properties to ask GWT to replace the System.getProperty() calls with the
+                        // values at compile time.
+                        // Note: these properties are set with the -setProperty GWT compiler argument when calling the
+                        // GWT plugin in the root pom.xml
                         projectModule.getSystemProperties()
                                 .stream().sorted()
                                 .forEach(p -> {
                                     Element propertyElement = document.createElement("define-configuration-property");
                                     propertyElement.setAttribute("name", p);
                                     propertyElement.setAttribute("is_multi_valued", "false");
-                                    nodeAppender.accept(propertyElement);
+                                    nodeAppenderIfNotOnlyComment.accept(propertyElement);
                                 });
                     }
                 });
@@ -107,6 +134,7 @@ public final class GwtModuleFile extends XmlModuleFile {
     }
 
     private static String getGwtModuleName(Module module) {
+        // TODO Move this hardcoded declaration into webfx.xml files (<gwt-module-name> tag)
         switch (module.getName()) {
             case "gwt-user": return "com.google.gwt.user.User";
             case "java-logging": return "com.google.gwt.logging.Logging";
