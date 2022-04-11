273653f89601dbdad143e2f003ea13419c5ab454
Bruno Salmon <dev.salmonb@gmail.com>
2022-04-11 16:23:52 +0100

    Added text file transaction mechanism (files are actually written only at the end if no exception have been raised)

--- src/main/java/dev/webfx/buildtool/util/textfile/TextFileReaderWriter.java
+++ src/main/java/dev/webfx/buildtool/util/textfile/TextFileReaderWriter.java
@@ -20,34 +20,13 @@ public class TextFileReaderWriter {
         }
     }
 
-    public static void writeTextFile(String content, Path path) {
-        try {
-            boolean exists = Files.exists(path);
-            if (!exists)
-                Files.createDirectories(path.getParent()); // Creating all necessary directories
-            BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
-            writer.write(content);
-            writer.flush();
-            writer.close();
-            Logger.log((exists ? "Updated " :  "Wrote " ) + path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void writeTextFileIfNewOrModified(String content, Path path) {
         writeTextFileIfNewOrModified(content, readTextFile(path), path);
     }
 
     public static void writeTextFileIfNewOrModified(String newContent, String oldContent, Path path) {
-        if (newContent == null)
-            try {
-                Files.deleteIfExists(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        else if (!areTextFileContentsIdentical(newContent, oldContent))
-           writeTextFile(newContent, path);
+        if (newContent == null && Files.exists(path) || !areTextFileContentsIdentical(newContent, oldContent))
+            writeTextFile(newContent, path);
     }
 
     private static boolean areTextFileContentsIdentical(String content1, String content2) {
@@ -57,4 +36,31 @@ public class TextFileReaderWriter {
     private static String removeLR(String content) {
         return content == null ? null : content.replaceAll("\r", "");
     }
+
+    public static void writeTextFile(String content, Path path) {
+        try (TextFileThreadTransaction transaction = TextFileThreadTransaction.open()) {
+            transaction.addOperation(new TextFileOperation(path, content));
+            transaction.commit(); // Executed now if not embed in a wider transaction, or later on commit of the wider transaction
+        }
+    }
+
+    static void commit(TextFileOperation op) {
+        try {
+            if (op.content == null) // Delete file
+                Files.deleteIfExists(op.path);
+            else { // Write file
+                boolean exists = Files.exists(op.path);
+                if (!exists)
+                    Files.createDirectories(op.path.getParent()); // Creating all necessary directories
+                BufferedWriter writer = Files.newBufferedWriter(op.path, StandardCharsets.UTF_8);
+                writer.write(op.content);
+                writer.flush();
+                writer.close();
+                Logger.log((exists ? "Updated " :  "Created " ) + op.path);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
