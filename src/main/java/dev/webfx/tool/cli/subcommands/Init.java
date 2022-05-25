03405b5580b501ede90ada26214f006bff2baf19
Bruno Salmon <dev.salmonb@gmail.com>
2022-05-25 19:18:23 +0100

    Changed init command to use groupId:artifactId:version syntax

--- src/main/java/dev/webfx/tool/cli/subcommands/Init.java
+++ src/main/java/dev/webfx/tool/cli/subcommands/Init.java
@@ -2,7 +2,6 @@ package dev.webfx.tool.cli.subcommands;
 
 import dev.webfx.tool.cli.core.DevRootModule;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.nio.file.Path;
@@ -14,29 +13,28 @@ import java.util.concurrent.Callable;
 @Command(name = "init", description = "Initialize a WebFX repository.")
 public final class Init extends CommonSubcommand implements Callable<Void> {
 
-    @Option(names = {"-i", "--inline"}, description = "Inline the WebFX parent pom instead of referencing it.")
-    private boolean inline;
+    /*@Option(names = {"-i", "--inline"}, description = "Inline the WebFX parent pom instead of referencing it.")
+    private boolean inline;*/
 
-    @Parameters(paramLabel = "groupId", description = "GroupId of the project artifact.")
-    private String groupId;
-
-    @Parameters(paramLabel = "artifactId", description = "ArtifactId of the project artifact.")
-    private String artifactId;
-
-    @Parameters(paramLabel = "version", description = "Version of the project artifact.")
-    private String version;
+    @Parameters(paramLabel = "artifact", description = "The project artifact expressed as groupId:artifactId:version")
+    private String artifact;
 
     @Override
     public Void call() {
         Path projectDirectoryPath = getProjectDirectoryPath();
         setWorkspaceDirectoryPath(projectDirectoryPath.getParent());
+        String[] split = artifact.split(":");
+        int i = 0, n = split.length;
+        String groupId = split[i++];
+        String artifactId = n > 1 ? split[i++] : null;
+        String version = split[i];
         if (artifactId == null)
             artifactId = projectDirectoryPath.getFileName().toString();
         DevRootModule module = (DevRootModule) getModuleRegistry().getOrCreateDevProjectModule(projectDirectoryPath);
         module.setGroupId(groupId);
         module.setArtifactId(artifactId);
         module.setVersion(version);
-        module.setInlineWebFxParent(inline);
+        //module.setInlineWebFxParent(inline);
         module.getWebFxModuleFile().writeFile();
         module.getMavenModuleFile().writeFile();
         module.getMavenModuleFile().updateAndWrite();
