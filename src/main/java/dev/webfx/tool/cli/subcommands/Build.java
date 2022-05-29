ba2e18596a4dcf6bf89954fa898b6be4fa33f229
Bruno Salmon <dev.salmonb@gmail.com>
2022-05-29 15:51:45 +0100

    Added options for Build command

--- src/main/java/dev/webfx/tool/cli/subcommands/Build.java
+++ src/main/java/dev/webfx/tool/cli/subcommands/Build.java
@@ -1,6 +1,7 @@
 package dev.webfx.tool.cli.subcommands;
 
 import dev.webfx.tool.cli.core.MavenCaller;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 /**
@@ -9,8 +10,23 @@ import picocli.CommandLine.Command;
 @Command(name = "build", description = "Invoke Maven build.")
 public final class Build extends CommonSubcommand implements Runnable {
 
+    @CommandLine.Option(names = {"--fatjar"}, description = "Creates a fat jar for the OpenJFX version", defaultValue = "true")
+    private boolean fatjar;
+
+    @CommandLine.Option(names = {"--gwt"}, description = "Includes the GWT compilation", defaultValue = "true")
+    private boolean gwt;
+
+    @CommandLine.Option(names = {"--desktop"}, description = "Includes the GWT compilation", defaultValue = "false")
+    private boolean desktop;
+
+
     @Override
     public void run() {
-        MavenCaller.invokeMavenGoal("package -P gwt-compile -P openjfx-fatjar");
+        MavenCaller.invokeMavenGoal("package " +
+                (fatjar ? "-P openjfx-fatjar " : "") +
+                (gwt ? "-P gwt-compile " : "") +
+                (desktop ? "-P gluon-desktop " : "") +
+                ""
+        );
     }
 }
