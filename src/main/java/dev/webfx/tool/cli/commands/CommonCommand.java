7fddf2afb68126486d30b82da73656c20556adde
Bruno Salmon <dev.salmonb@gmail.com>
2022-06-01 17:46:47 +0100

    Fixed WebFX repository check: could mistakenly raise an exception

--- src/main/java/dev/webfx/tool/cli/commands/CommonCommand.java
+++ src/main/java/dev/webfx/tool/cli/commands/CommonCommand.java
@@ -163,7 +163,7 @@ public class CommonCommand {
 
     protected Module getWorkingModule() {
         if (moduleProject == null) {
-            if (topRootDirectoryPath == null)
+            if (getTopRootDirectoryPath() == null)
                 throw new WebFxCliException("Not a WebFX repository (or any of the parent directories): no webfx.xml");
             String moduleName = getModuleName();
             if (moduleName == null)
