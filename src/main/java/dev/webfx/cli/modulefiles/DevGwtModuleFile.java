97bff0336fe0f1429ecfb43dba0ea079946fb425
Bruno Salmon <dev.salmonb@gmail.com>
2022-07-13 21:59:15 +0100

    Fixed CLI not producing the same GWT module whether ran under individual repository or contrib repository

--- src/main/java/dev/webfx/cli/modulefiles/DevGwtModuleFile.java
+++ src/main/java/dev/webfx/cli/modulefiles/DevGwtModuleFile.java
@@ -1,12 +1,11 @@
 package dev.webfx.cli.modulefiles;
 
-import dev.webfx.cli.core.DevProjectModule;
+import dev.webfx.cli.core.*;
 import dev.webfx.cli.core.Module;
-import dev.webfx.cli.core.ModuleDependency;
-import dev.webfx.cli.core.ProjectModule;
 import dev.webfx.cli.modulefiles.abstr.DevXmlModuleFileImpl;
 import dev.webfx.cli.util.textfile.ResourceTextFileReader;
 import dev.webfx.cli.util.xml.XmlUtil;
+import dev.webfx.lib.reusablestream.ReusableStream;
 import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -44,10 +43,11 @@ public final class DevGwtModuleFile extends DevXmlModuleFileImpl {
                 .stream().sorted(Map.Entry.comparingByKey())
                 .forEach(moduleGroup -> {
                     Module module = moduleGroup.getKey();
+                    String moduleName = module.getName();
                     // Ignoring emulated modules for 2 reasons:
                     // 1) they are destined to the super source, not the source (so they don't need to be listed here)
                     // 2) these modules have been shaded so the original source packages would start with emul (which would be incorrect) if they were listed here
-                    if (module.getName().startsWith("webfx-platform-emul-") && module.getName().endsWith("-gwt"))
+                    if (moduleName.startsWith("webfx-platform-emul-") && moduleName.endsWith("-gwt"))
                         return;
                     Node parentNode = moduleSourceEndNode.getParentNode();
                     // Creating a node appender that inserts a node with the after a new line and indentation
@@ -75,7 +75,7 @@ public final class DevGwtModuleFile extends DevXmlModuleFileImpl {
                         }
                         nodeAppender.accept(node);
                     };
-                    nodeAppenderIfNotOnlyComment.accept(document.createComment(createModuleSectionLine(module.getName())));
+                    nodeAppenderIfNotOnlyComment.accept(document.createComment(createModuleSectionLine(moduleName)));
                     /* Commented because this "Used by " section doesn't always generate the same content (ex: different result when executed on a single repository or on the contrib repository)
                     moduleGroup.getValue()
                             .stream().sorted(Comparator.comparing(ModuleDependency::getSourceModule)) // Sorting by source module name instead of default (destination module name)
@@ -87,39 +87,51 @@ public final class DevGwtModuleFile extends DevXmlModuleFileImpl {
                         inherits.setAttribute("name", gwtModuleName);
                         nodeAppenderIfNotOnlyComment.accept(inherits);
                     }
+                    ReusableStream<String> sourcePackages = ReusableStream.empty(), resourcePackages = sourcePackages, systemProperties = sourcePackages;
                     if (module instanceof ProjectModule) {
                         ProjectModule projectModule = (ProjectModule) module;
-                        projectModule.getJavaSourcePackages()
-                                .sorted()
-                                .forEach(p -> {
-                                    Element sourceElement = document.createElement("source");
-                                    sourceElement.setAttribute("path", p.replaceAll("\\.", "/"));
-                                    nodeAppenderIfNotOnlyComment.accept(sourceElement);
-                                });
-                        projectModule.getResourcePackages()
-                                .sorted()
-                                .forEach(p -> {
-                                    Element resourceElement = document.createElement("resource");
-                                    resourceElement.setAttribute("path", p.replaceAll("\\.", "/"));
-                                    nodeAppenderIfNotOnlyComment.accept(resourceElement);
-                                    Element publicElement = document.createElement("public");
-                                    publicElement.setAttribute("path", "");
-                                    publicElement.setAttribute("includes", p.replaceAll("\\.", "/") + "/");
-                                    nodeAppenderIfNotOnlyComment.accept(publicElement);
-                                });
-                        // Declaring the system properties to ask GWT to replace the System.getProperty() calls with the
-                        // values at compile time.
-                        // Note: these properties are set with the -setProperty GWT compiler argument when calling the
-                        // GWT plugin in the root pom.xml
-                        projectModule.getSystemProperties()
-                                .sorted()
-                                .forEach(p -> {
-                                    Element propertyElement = document.createElement("define-configuration-property");
-                                    propertyElement.setAttribute("name", p);
-                                    propertyElement.setAttribute("is_multi_valued", "false");
-                                    nodeAppenderIfNotOnlyComment.accept(propertyElement);
-                                });
+                        resourcePackages = projectModule.getResourcePackages();
+                        sourcePackages = projectModule.getJavaSourcePackages();
+                        systemProperties = projectModule.getSystemProperties();
+                    } else if (module instanceof LibraryModule && !ModuleRegistry.isJdkModule(module)) {
+                        boolean isGwtLibrary = gwtModuleName != null && (
+                                        gwtModuleName.startsWith("com.google.gwt") ||
+                                        gwtModuleName.startsWith("org.gwtproject") ||
+                                        gwtModuleName.startsWith("elemental2")) ||
+                        moduleName.startsWith("jsinterop");
+                        if (!isGwtLibrary) // No need to list the source packages for GWT libraries
+                            sourcePackages = ((LibraryModule) module).getExportedPackages();
                     }
+                    sourcePackages
+                            .sorted()
+                            .forEach(p -> {
+                                Element sourceElement = document.createElement("source");
+                                sourceElement.setAttribute("path", p.replaceAll("\\.", "/"));
+                                nodeAppenderIfNotOnlyComment.accept(sourceElement);
+                            });
+                    resourcePackages
+                            .sorted()
+                            .forEach(p -> {
+                                Element resourceElement = document.createElement("resource");
+                                resourceElement.setAttribute("path", p.replaceAll("\\.", "/"));
+                                nodeAppenderIfNotOnlyComment.accept(resourceElement);
+                                Element publicElement = document.createElement("public");
+                                publicElement.setAttribute("path", "");
+                                publicElement.setAttribute("includes", p.replaceAll("\\.", "/") + "/");
+                                nodeAppenderIfNotOnlyComment.accept(publicElement);
+                            });
+                    // Declaring the system properties to ask GWT to replace the System.getProperty() calls with the
+                    // values at compile time.
+                    // Note: these properties are set with the -setProperty GWT compiler argument when calling the
+                    // GWT plugin in the root pom.xml
+                    systemProperties
+                            .sorted()
+                            .forEach(p -> {
+                                Element propertyElement = document.createElement("define-configuration-property");
+                                propertyElement.setAttribute("name", p);
+                                propertyElement.setAttribute("is_multi_valued", "false");
+                                nodeAppenderIfNotOnlyComment.accept(propertyElement);
+                            });
                 });
         return true;
     }
