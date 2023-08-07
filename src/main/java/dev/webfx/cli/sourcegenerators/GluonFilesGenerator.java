da03cc17fdf0f502e1da8872eee96c6f75154101
Bruno Salmon <dev.salmonb@gmail.com>
2023-08-07 14:29:52 +0100

    Removed duplicates when generating reflection.json

--- src/main/java/dev/webfx/cli/sourcegenerators/GluonFilesGenerator.java
+++ src/main/java/dev/webfx/cli/sourcegenerators/GluonFilesGenerator.java
@@ -4,6 +4,8 @@ import dev.webfx.cli.core.DevProjectModule;
 import dev.webfx.cli.core.ProjectModule;
 import dev.webfx.cli.util.textfile.TextFileReaderWriter;
 
+import java.util.Objects;
+
 /**
  * @author Bruno Salmon
  */
@@ -12,31 +14,27 @@ public final class GluonFilesGenerator {
     public static void generateGraalVmReflectionJson(DevProjectModule gluonModule) {
         StringBuilder sb = new StringBuilder();
         ProjectModule.filterProjectModules(gluonModule.getMainJavaSourceRootAnalyzer().getTransitiveModules())
-                .forEach(module -> {
-                    String json = module.getWebFxModuleFile().getGraalVmReflectionJson();
-                    if (json != null) {
-                        json = removeLineFeeds(json, true);
-                        if (json.startsWith("["))
-                            json = json.substring(1);
-                        if (json.endsWith("]"))
-                            json = json.substring(0, json.length() - 1);
-                        json = removeLineFeeds(json, false);
-                        if (!json.isEmpty()) {
-                            if (sb.length() > 0)
-                                sb.append(",");
-                            sb.append(json);
-                        }
+                .map(module -> module.getWebFxModuleFile().getGraalVmReflectionJson())
+                .filter(Objects::nonNull)
+                .distinct()
+                .forEach(json -> { // Json String (expecting a Json array [ ... ])
+                    // Removing the possible first empty lines
+                    json = json.replaceAll("\\n\\s+\\n", "\n");
+                    // Removing the brackets, because all arrays are merged into 1
+                    int openBracketIndex = json.indexOf('[');
+                    int closeBracketIndex = json.lastIndexOf(']');
+                    if (openBracketIndex > 0 && closeBracketIndex > 0) {
+                        json = json.substring(openBracketIndex + 1, closeBracketIndex);
+                        // Also removing left indentation (extra indentation can come from webfx.xml export)
+                        json = json.replace("\n" + " ".repeat(openBracketIndex), "\n");
+                    }
+                    if (!json.isEmpty()) {
+                        if (sb.length() > 0)
+                            sb.append(",");
+                        sb.append(json);
                     }
                 });
         TextFileReaderWriter.writeTextFileIfNewOrModified(sb.length() == 0 ? null : "[\n" + sb + "\n]", gluonModule.getHomeDirectory().resolve("src/main/graalvm_conf/reflection.json"));
-    }
-
-    private static String removeLineFeeds(String json, boolean includeWhiteSpaces) {
-        while (!json.isEmpty() && (json.startsWith("\n") || includeWhiteSpaces && Character.isWhitespace(json.charAt(0))))
-            json = json.substring(1);
-        while (!json.isEmpty() && (json.endsWith("\n") || includeWhiteSpaces && Character.isWhitespace(json.charAt(json.length() - 1))))
-            json = json.substring(0, json.length() - 1);
-        return json;
     }
 
 }
