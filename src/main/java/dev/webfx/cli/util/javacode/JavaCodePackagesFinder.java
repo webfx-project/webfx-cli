6400c1b3e2bdac2931e74243770368502bf5c05b
Bruno Salmon <dev.salmonb@gmail.com>
2023-09-18 11:17:56 +0100

    Added AST class recognition in Java packages finder (exception)

--- src/main/java/dev/webfx/cli/util/javacode/JavaCodePackagesFinder.java
+++ src/main/java/dev/webfx/cli/util/javacode/JavaCodePackagesFinder.java
@@ -19,7 +19,7 @@ public final class JavaCodePackagesFinder extends JavaCodePatternFinder {
      */
 
     private final static JavaCodePattern PACKAGE_PATTERN =
-            new JavaCodePattern(Pattern.compile("[\\s(<](?!this\\.)([a-z_0-9]+(\\.[a-z_0-9]+)*)(\\.(\\*;|(lr_parser|[A-Z]+[0-9]*[a-z]+[A-Za-z_0-9]*)[\\s()<>;.:\\[]))"), 1);
+            new JavaCodePattern(Pattern.compile("[\\s(<](?!this\\.)([a-z_0-9]+(\\.[a-z_0-9]+)*)(\\.(\\*;|(lr_parser|AST|[A-Z]+[0-9]*[a-z]+[A-Za-z_0-9]*)[\\s()<>;.:\\[]))"), 1);
 
     public JavaCodePackagesFinder(JavaCode javaCode) {
         super(PACKAGE_PATTERN, javaCode);
