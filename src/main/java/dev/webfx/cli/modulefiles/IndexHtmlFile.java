c15baa0a7365dce748fe4d618bbe26530ac92b43
Bruno Salmon <dev.salmonb@gmail.com>
2025-11-26 14:12:39 +0000

    Updated index.html generation for PWA

--- src/main/java/dev/webfx/cli/modulefiles/IndexHtmlFile.java
+++ src/main/java/dev/webfx/cli/modulefiles/IndexHtmlFile.java
@@ -27,12 +27,112 @@ public class IndexHtmlFile extends DevModuleFileImpl {
     @Override
     public boolean writeFile() {
         StringBuilder headSb = new StringBuilder(), bodySb = new StringBuilder();
-        if (getProjectModule().isPwa()) {
-            headSb.append("        <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'>\n");
-            headSb.append("        <link rel='manifest' href='./pwa-manifest.json'>\n");
+        DevProjectModule module = getModule();
+        if (module.isPwa()) {
+            headSb.append("""
+                <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'>
+                <link rel='manifest' href='./pwa-manifest.json'>
+                <style>
+                    #pwa-progress-bar-container {
+                        display: none;
+                        position: fixed;
+                        top: 50%;
+                        left: 25%;
+                        width: 50%;
+                        height: 10px;
+                        border: 1px lightgray solid;
+                        color: transparent;
+                    }
+                    #pwa-progress-bar {
+                        width: 0;
+                        height: 100%;
+                        background-color: #007bff;
+                        transition: width 0.2s ease;
+                    }
+                </style>""".indent(8));
+            bodySb.append("""
+                    <div id="pwa-progress-bar-container">
+                        <div id="pwa-progress-bar">This is to force the browser to paint this new page</div>
+                    </div>
+               
+                    <script>
+                        function loadGwtApp() {
+                            console.log('Injecting application script...');
+                            var script = document.createElement('script');
+                            script.src = "./%APP_SCRIPT_SRC%";
+                            document.body.appendChild(script);
+                        }
+                        if ('serviceWorker' in navigator) {
+                            navigator.serviceWorker.register('pwa-service-worker.js');
+                            let isCriticalDone = false;
+                            function checkLaunch() {
+                                if (isCriticalDone && navigator.serviceWorker.controller) {
+                                    loadGwtApp();
+                                }
+                            }
+                            navigator.serviceWorker.addEventListener('message', event => {
+                                if (event.data) {
+                                    if (event.data.type === 'loading_progress') {
+                                        const percent = Math.round((event.data.current / event.data.total) * 100);
+                                        const container = document.getElementById('pwa-progress-bar-container');
+                                        const bar = document.getElementById('pwa-progress-bar');
+                                        if (container && bar) {
+                                            if (!event.data.completed && percent < 100) {
+                                                container.style.display = 'block';
+                                                bar.style.width = percent + '%';
+                                            }
+                                            if (event.data.completed || percent >= 100) {
+                                                bar.style.width = '100%';
+                                                setTimeout(() => container.style.display = 'none', 500);
+                                            }
+                                        }
+                                        if (event.data.criticalCompleted && !isCriticalDone) {
+                                            isCriticalDone = true;
+                                            checkLaunch();
+                                        }
+                                    } else if (event.data.type === 'status') {
+                                        if (event.data.criticalCompleted && !isCriticalDone) {
+                                            isCriticalDone = true;
+                                            checkLaunch();
+                                        }
+                                    }
+                                }
+                            });
+                            if (navigator.serviceWorker.controller) {
+                                navigator.serviceWorker.controller.postMessage({ type: 'check_status' });
+                            } else {
+                                navigator.serviceWorker.addEventListener('controllerchange', () => {
+                                    navigator.serviceWorker.controller.postMessage({ type: 'check_status' });
+                                });
+                            }
+                        } else {
+                            loadGwtApp();
+                        }
+                    </script>
+               """.replace("%APP_SCRIPT_SRC%", getApplicationJsScriptFileName(module)).indent(8));
+        } else {
+            bodySb.append("""
+                <script>
+                    async function unregisterPwaServiceWorker() {
+                        if (navigator.serviceWorker) {
+                            navigator.serviceWorker.getRegistrations()
+                                .then(registrations => {
+                                    registrations.forEach((registration, i) => registration.unregister());
+                                    return null;
+                                });
+                            const cache = await caches.open("webfx-pwa-cache")
+                            const keys = await cache.keys();
+                            await Promise.all(keys.map(async (req) => {
+                                await cache.delete(req);
+                            }));
+                        }
+                    }
+                    unregisterPwaServiceWorker();
+                </script>
+                """.indent(8));
         }
         ReusableStream<ProjectModule> transitiveProjectModules =
-                ProjectModule.filterProjectModules(getProjectModule().getMainJavaSourceRootAnalyzer().getThisAndTransitiveModules()).distinct();
+            ProjectModule.filterProjectModules(module.getMainJavaSourceRootAnalyzer().getThisAndTransitiveModules()).distinct();
         // Fixing possible incomplete stream
         Workaround.fixTerminalReusableStream(transitiveProjectModules); // TODO: remove this once fixed
         Path mainCssPath = getModule().getWebAppSourceDirectory().resolve(MAIN_CSS_RELATIVE_PATH);
@@ -41,97 +141,97 @@ public class IndexHtmlFile extends DevModuleFileImpl {
         ReusableStream.concat(
                 transitiveProjectModules.flatMap(m -> m.getWebFxModuleFile().getHtmlNodes()),
                 // JS script (generated by GWT or J2CL)
-                ReusableStream.of(XmlUtil.lookupElement(XmlUtil.parseXmlString("<html><body order='0'><script type='text/javascript' charset='utf-8' src='./" + getGeneratedJsFileName() + "'/></body></html>").getRootElement(), "/html[1]")),
+                module.isPwa() ? null : ReusableStream.of(XmlUtil.lookupElement(XmlUtil.parseXmlString("<html><body order='0'><script type='text/javascript' charset='utf-8' src='./" + getApplicationJsScriptFileName(module) + "'/></body></html>").getRootElement(), "/html[1]")),
                 // Main CSS (if present). We also add the maven build timestamp as a query parameter to force the browser to reload the CSS on each new build
                 isMainCssPresent ? ReusableStream.of(XmlUtil.lookupElement(XmlUtil.parseXmlString("<html><head><link rel='stylesheet' href='./" + MAIN_CSS_RELATIVE_PATH + "?v=${maven.build.timestamp}'></link></head></html>").getRootElement(), "/html[1]")) : null
-        )
-                .filter(htmlNode -> checkNodeConditions(htmlNode, transitiveProjectModules))
-                .flatMap(htmlNode -> htmlNode == null ? ReusableStream.empty() : XmlUtil.nodeListToReusableStream(htmlNode.content(), n -> n))
-                .filter(Element.class::isInstance).map(Element.class::cast)
-                .sorted(Comparator.comparingInt(IndexHtmlFile::getNodeOrder))
-                .filter(headOrBodyNode -> checkNodeConditions(headOrBodyNode, transitiveProjectModules))
-                .forEach(headOrBodyNode -> {
-                    String nodeName = headOrBodyNode.getName();
-                    StringBuilder sb = "head".equalsIgnoreCase(nodeName) ? headSb : "body".equalsIgnoreCase(nodeName) ? bodySb : null;
-                    if (sb != null) {
-                        String xmlText = XmlUtil.formatHtmlText(headOrBodyNode);
-                        // Removing the head or body tag
-                        xmlText = xmlText.substring(xmlText.indexOf('>') + 1);
-                        xmlText = xmlText.substring(0, xmlText.length() - 3 - nodeName.length());
-                        xmlText = xmlText.replaceAll("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">", "");
-                        Scanner scanner = new Scanner(xmlText);
-                        boolean firstEmptyLineReached = false;
-                        int shift = 0, emptyLines = 0;
-                        while (scanner.hasNextLine()) {
-                            String line = scanner.nextLine();
-                            boolean blank = line.isBlank();
-                            if (!blank || firstEmptyLineReached) {
-                                if (!firstEmptyLineReached) {
-                                    shift = 8 - line.indexOf("<");
-                                    firstEmptyLineReached = true;
-                                } else if (blank) {
-                                    emptyLines++;
-                                    continue;
-                                } else {
-                                    sb.append("\n".repeat(emptyLines));
-                                    emptyLines = 0;
-                                }
-                                int firstChar = 0;
-                                if (shift >= 0)
-                                    sb.append(" ".repeat(shift));
-                                else {
-                                    firstChar = Math.min(-shift, line.length());
-                                    for (int i = 0; i < firstChar; i++)
-                                        if (!Character.isWhitespace(line.charAt(i))) {
-                                            firstChar = i;
-                                            break;
-                                        }
-                                }
-                                sb.append(line.substring(firstChar)).append('\n');
+            )
+            .filter(htmlNode -> checkNodeConditions(htmlNode, transitiveProjectModules))
+            .flatMap(htmlNode -> htmlNode == null ? ReusableStream.empty() : XmlUtil.nodeListToReusableStream(htmlNode.content(), n -> n))
+            .filter(Element.class::isInstance).map(Element.class::cast)
+            .sorted(Comparator.comparingInt(IndexHtmlFile::getNodeOrder))
+            .filter(headOrBodyNode -> checkNodeConditions(headOrBodyNode, transitiveProjectModules))
+            .forEach(headOrBodyNode -> {
+                String nodeName = headOrBodyNode.getName();
+                StringBuilder sb = "head".equalsIgnoreCase(nodeName) ? headSb : "body".equalsIgnoreCase(nodeName) ? bodySb : null;
+                if (sb != null) {
+                    String xmlText = XmlUtil.formatHtmlText(headOrBodyNode);
+                    // Removing the head or body tag
+                    xmlText = xmlText.substring(xmlText.indexOf('>') + 1);
+                    xmlText = xmlText.substring(0, xmlText.length() - 3 - nodeName.length());
+                    xmlText = xmlText.replaceAll("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">", "");
+                    Scanner scanner = new Scanner(xmlText);
+                    boolean firstEmptyLineReached = false;
+                    int shift = 0, emptyLines = 0;
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        boolean blank = line.isBlank();
+                        if (!blank || firstEmptyLineReached) {
+                            if (!firstEmptyLineReached) {
+                                shift = 8 - line.indexOf("<");
+                                firstEmptyLineReached = true;
+                            } else if (blank) {
+                                emptyLines++;
+                                continue;
+                            } else {
+                                sb.append("\n".repeat(emptyLines));
+                                emptyLines = 0;
                             }
+                            int firstChar = 0;
+                            if (shift >= 0)
+                                sb.append(" ".repeat(shift));
+                            else {
+                                firstChar = Math.min(-shift, line.length());
+                                for (int i = 0; i < firstChar; i++)
+                                    if (!Character.isWhitespace(line.charAt(i))) {
+                                        firstChar = i;
+                                        break;
+                                    }
+                            }
+                            sb.append(line.substring(firstChar)).append('\n');
                         }
                     }
-                });
-        if (getModule().getBuildInfo().isForTeaVm) {
-            if (getModule().isWasmModule()) {
+                }
+            });
+        if (module.getBuildInfo().isForTeaVm) {
+            if (module.isWasmModule()) {
                 bodySb.append("""
-                <script>
-                    async function main() {
-                        let teavm = await TeaVM.wasmGC.load("classes.wasm", {
-                            stackDeobfuscator: {
-                                enabled: false // Can be set to true during development to get clear stack traces
-                            }
-                        });
-                        teavm.exports.main([]);
-                    }
-                    main();
-                </script>
-                """.indent(8));
+                    <script>
+                        async function main() {
+                            let teavm = await TeaVM.wasmGC.load("classes.wasm", {
+                                stackDeobfuscator: {
+                                    enabled: false // Can be set to true during development to get clear stack traces
+                                }
+                            });
+                            teavm.exports.main([]);
+                        }
+                        main();
+                    </script>
+                    """.indent(8));
             } else
                 bodySb.append("        <script>main()</script>");
         }
         String html = ResourceTextFileReader.readTemplate("index.html")
-                .replace("${generatedHeadContent}", headSb)
-                .replace("${generatedBodyContent}", bodySb);
+            .replace("${generatedHeadContent}", headSb)
+            .replace("${generatedBodyContent}", bodySb);
         TextFileReaderWriter.writeTextFileIfNewOrModified(html, getModuleFilePath());
         // Also updating index.html in target if exists (so the user doesn't have to recompile the app for just a style change)
-        Path targetIndexHtmlPath = getProjectModule().getGwtExecutableFilePath();
+        Path targetIndexHtmlPath = module.getGwtExecutableFilePath();
         if (Files.exists(targetIndexHtmlPath))
             TextFileReaderWriter.writeTextFileIfNewOrModified(html, targetIndexHtmlPath);
         return true;
     }
 
-    private String getGeneratedJsFileName() {
-        BuildInfo buildInfo = getModule().getBuildInfo();
+    public static String getApplicationJsScriptFileName(DevProjectModule module) {
+        BuildInfo buildInfo = module.getBuildInfo();
         if (buildInfo.isForTeaVm)
-            return getModule().isWasmModule() ? "classes.wasm-runtime.js" : "classes.js";
+            return module.isWasmModule() ? "classes.wasm-runtime.js" : "classes.js";
         if (buildInfo.isForJ2cl)
-            return getModule().getName() + ".js";
+            return module.getName() + ".js";
         // GWT
-        return getModule().getName().replaceAll("-", "_") + ".nocache.js";
+        return module.getName().replaceAll("-", "_") + ".nocache.js";
     }
 
-    private static boolean checkNodeConditions(Element headOrBodyNode, ReusableStream<ProjectModule> transitiveProjectModules) {
+        private static boolean checkNodeConditions(Element headOrBodyNode, ReusableStream<ProjectModule> transitiveProjectModules) {
         String ifModulePropertyTrue = XmlUtil.getAttributeValue(headOrBodyNode, "if-module-property-true");
         if (ifModulePropertyTrue != null && transitiveProjectModules.noneMatch(m -> m.getWebFxModuleFile().getModuleProperties().anyMatch(p -> p.getPropertyName().equals(ifModulePropertyTrue) && "true".equalsIgnoreCase(p.getPropertyValue()))))
             return false;
